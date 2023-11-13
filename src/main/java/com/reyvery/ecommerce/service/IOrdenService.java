06d3b4bc3708dc892fc2b53126c037e9f297c1d9
Ronald Urbano <sam.ronaldurbano@gmail.com>
2023-11-13 02:03:43 -0500

    vista detalle compra y funcionalidad

--- src/main/java/com/reyvery/ecommerce/service/IOrdenService.java
+++ src/main/java/com/reyvery/ecommerce/service/IOrdenService.java
@@ -1,6 +1,7 @@
 package com.reyvery.ecommerce.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -10,7 +11,9 @@ import com.reyvery.ecommerce.model.Usuario;
 @Service
 public interface IOrdenService {
 	List<Orden> findAll();
+	Optional<Orden> findById(Integer id);
  Orden save (Orden orden);
  String generarNumeroOrden();
  List<Orden> findByUsuario(Usuario usuario);
+ 
 }
