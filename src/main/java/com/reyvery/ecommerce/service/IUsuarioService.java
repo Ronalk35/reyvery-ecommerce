29504c4d842b4e88bbc02865fc2d604b0dd0ff1d
Ronald Urbano <sam.ronaldurbano@gmail.com>
2023-11-12 21:34:32 -0500

    Función de registro de usuario

--- src/main/java/com/reyvery/ecommerce/service/IUsuarioService.java
+++ src/main/java/com/reyvery/ecommerce/service/IUsuarioService.java
@@ -6,4 +6,5 @@ import com.reyvery.ecommerce.model.Usuario;
 
 public interface IUsuarioService {
 Optional<Usuario> findById(Integer id);
+Usuario save(Usuario usuario);
 }
--- src/main/java/com/reyvery/ecommerce/service/UsuarioServiceImpl.java
+++ src/main/java/com/reyvery/ecommerce/service/UsuarioServiceImpl.java
@@ -18,6 +18,12 @@ public class UsuarioServiceImpl implements IUsuarioService {
 	
 		return usuarioRepository.findById(id) ;
 	}
+
+	@Override
+	public Usuario save(Usuario usuario) {
+		
+		return usuarioRepository.save(usuario);
+	}
 	
 	
 
