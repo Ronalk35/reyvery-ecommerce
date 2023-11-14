36e8bcdae7ed136cf0b2c95f6361079e640bd178
Ronald Urbano <sam.ronaldurbano@gmail.com>
2023-11-13 20:06:59 -0500

    vista administrador ordenes para ver las ordenes generadas

--- src/main/java/com/reyvery/ecommerce/controller/AdministradorController.java
+++ src/main/java/com/reyvery/ecommerce/controller/AdministradorController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.reyvery.ecommerce.service.IOrdenService;
 import com.reyvery.ecommerce.service.IUsuarioService;
 import com.reyvery.ecommerce.service.ProductoService;
 import com.reyvery.ecommerce.model.Producto;
@@ -27,6 +28,9 @@ public class AdministradorController {
 	@Autowired
 	private IUsuarioService usuarioService;
 	
+	@Autowired
+	private IOrdenService ordenService;
+	
 	@GetMapping("")
 	public String home(Model model) {
 		
@@ -40,5 +44,10 @@ public class AdministradorController {
 		model.addAttribute("usuarios", usuarioService.findAll());
 		
 		return "administrador/usuarios";
+	}
+	@GetMapping("/ordenes")
+	public String ordenes(Model model) {
+		model.addAttribute("ordenes", ordenService.findAll());
+		return "administrador/ordenes";
 	}
 }
